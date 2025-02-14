e33ec46d2d7cd34b4a6c151ae307de3f35626f3d
Ibrahim Attia <75019839+ibra-san@users.noreply.github.com>
2025-02-14 21:30:34 +0400

    Intialized the project

---
+++ my-app/app/page.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function page() {
+  return (
+    <section className="py-24">
+      <div className="container">
+        <h1 className="text-3xl font-bold">
+          Next Js Starter
+        </h1>
+      </div>
+    </section>
+  )
+}
+
+export default page
